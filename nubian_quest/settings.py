d009c0a3149722602a5254d4a95196994ef1435e
Bahij Youssef <info@virtuestudios.co.uk>
2018-11-18 21:34:44 +0000

    Added Shop and Contact pages. Made a few tweaks to base template as well as navbar. Added PostgreSQL settings.

--- nubian_quest/settings.py
+++ nubian_quest/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'nubian_quest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nqdb',
+        'USER': 'postgres',
+        'PASSWORD': 'By240788',
+        'HOST': 'localhost',
     }
 }
 
--- pages/views.py
+++ pages/views.py
@@ -10,18 +10,33 @@ cloudinary.config(
 def index(request):
 
     context = {
-        'book': cloudinary.CloudinaryImage('nubian_quest/book.png').build_url(secure=True, quality=20),
-        'image3': cloudinary.CloudinaryImage('nubian_quest/image3.jpg').build_url(secure=True, quality=20, height=800, crop='fit'),
-        'image1_original': cloudinary.CloudinaryImage('nubian_quest/image1_original.jpg').build_url(secure=True, quality=20, height=800, crop='fit'),
-        'image2_original': cloudinary.CloudinaryImage('nubian_quest/image2_original.jpg').build_url(secure=True, quality=20, height=800, crop='fit'),
-        'image7': cloudinary.CloudinaryImage('nubian_quest/image7.jpg').build_url(secure=True, quality=20, height=800, crop='fit'),
-        'image4': cloudinary.CloudinaryImage('nubian_quest/image4.jpg').build_url(secure=True, quality=20, height=800, crop='fit'),
-        'image5': cloudinary.CloudinaryImage('nubian_quest/image5.jpg').build_url(secure=True, quality=20, height=800, crop='fit'),
-        'image8': cloudinary.CloudinaryImage('nubian_quest/image8.jpg').build_url(secure=True, quality=20, height=800, crop='fit'),
+        'book': cloudinary.CloudinaryImage('nubian_quest/book.png').build_url(secure=True, quality=60),
+        'image3': cloudinary.CloudinaryImage('nubian_quest/image3.jpg').build_url(secure=True, quality=60, height=800, crop='fit', fetch_format='auto'),
+        'image1_original': cloudinary.CloudinaryImage('nubian_quest/image1_original.jpg').build_url(secure=True, quality=60, height=800, crop='fit', fetch_format='auto'),
+        'image2_original': cloudinary.CloudinaryImage('nubian_quest/image2_original.jpg').build_url(secure=True, quality=60, height=800, crop='fit', fetch_format='auto'),
+        'image7': cloudinary.CloudinaryImage('nubian_quest/image7.jpg').build_url(secure=True, quality=60, height=800, crop='fit', fetch_format='auto'),
+        'image4': cloudinary.CloudinaryImage('nubian_quest/image4.jpg').build_url(secure=True, quality=60, height=800, crop='fit', fetch_format='auto'),
+        'image5': cloudinary.CloudinaryImage('nubian_quest/image5.jpg').build_url(secure=True, quality=60, height=800, crop='fit', fetch_format='auto'),
+        'image8': cloudinary.CloudinaryImage('nubian_quest/image8.jpg').build_url(secure=True, quality=60, height=800, crop='fit', fetch_format='auto'),
     }
 
     return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+
+    context = {
+        'book': cloudinary.CloudinaryImage('nubian_quest/book.png').build_url(secure=True, quality=40),
+        'author': cloudinary.CloudinaryImage('nubian_quest/author.jpg').build_url(secure=True, quality=40),
+    }
+
+    return render(request, 'pages/about.html', context)
+
+
+def shop(request):
+
+    return render(request, 'pages/shop.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
